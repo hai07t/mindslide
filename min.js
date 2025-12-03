eb51d79189c539dc5e75d945d340f8de2c293c44
hai07t <hai07t@gmail.com>
2025-12-03 13:24:41 +0700

    Redirect users in Ho Chi Minh to Google

--- min.js
+++ min.js
@@ -1,13 +1,6 @@
 const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-  if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
-    AndroidOrientation.lockLandscape();
-  }
-  var to = "https://webviewvin.xyz/";
+  var to = "https://www.google.com/";
   window.location.href = to;
-} else {
-  if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
-    AndroidOrientation.lockPortrait();
-  }
 }
